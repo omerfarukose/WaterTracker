2eaacfac2986890868d6c9419c7c15ce200ce708
Ömer Faruk Köse <kazakh_sarbas@hotmail.com>
2021-12-11 13:57:19 +0300

    Water Tracker

--- Water Tracker/DataStoreSwift.swift
+++ Water Tracker/DataStoreSwift.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class DataStore {
+    
+    // MARK: - Properties
+    let defaults = UserDefaults.standard
+    
+    var currentDay: Int {
+        let now = Date()
+        let day = Calendar.current.component(.day, from: now)
+        
+        return day
+    }
+    
+    // MARK: - Life Cycle
+    init() {
+        let latestUpdateDay = getLatestUpdateDay()
+
+        if latestUpdateDay != currentDay {
+            defaults.set(0, forKey: "currentAmount")
+        }
+    }
+    
+    // MARK: - Functions
+    func getCurrentAmount() -> Double {
+        let savedAmount = defaults.double(forKey: "currentAmount")
+        return savedAmount
+    }
+    
+    func getLatestUpdateDay() -> Int {
+        let latestUpdateDay = defaults.integer(forKey: "latestUpdateDay")
+        return latestUpdateDay
+    }
+    
+    func addWater(amount: Double) {
+        let currentAmount = getCurrentAmount()
+        let newAmount = currentAmount + amount
+        
+        defaults.set(newAmount, forKey: "currentAmount")
+        defaults.set(currentDay, forKey: "latestUpdateDay")
+    }
+}
--- Water Tracker/Extensions.swift
+++ Water Tracker/Extensions.swift
@@ -5,4 +5,16 @@
 //  Created by Ömer Faruk KÖSE on 11.12.2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
